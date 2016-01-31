a7d4ba26dbf0598576547d831fc70ae063e79422
panda2134 <smallpanda2134@gmail.com>
2016-01-31 16:10:19 +0800

    Update oi-lib.h

--- oi-lib.h
+++ oi-lib.h
@@ -8,5 +8,30 @@ namespace OIlib
 	bool odd(int);
 	bool even(int);
 	int gcd(int,int);
+	class Node{
+		private:
+			Node *next;
+			int id;
+			string val;
+		public:
+			Node* getNext();
+			void setNext(Node*);
+			void setValue(string v);
+			string getValue();
+			void ins(string value);
+			void del();
+};
+	class NodeList{
+		private:
+			Node *head,*cur;
+		public:
+			NodeList();
+			~NodeList();
+			Node* getHead();
+			Node* getCur();
+			void goNext();
+			void ins(string value);
+			void rewind();
+};
 }
 #endif
